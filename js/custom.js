cc3159fd82c15d5063fcfe5eb2819d79b97008a4
Sriram Sabarish <sabarish@Srirams-MacBook-Pro.local>
2019-05-18 19:22:59 +0100

    Final documents

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+//This file is used to display the toggle menu for mobile devices
+
+//Toggle Menu Starts here
+function myFunction() {
+  var x = document.getElementById("navigation");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+//Toggle Menu ends here
